9ee84abb76a2b78c3088e6a5503cfb7ff759fdf1
malcolmtsang <88289179+malcolmtsang@users.noreply.github.com>
2021-08-07 18:08:09 +1000

    Add files via upload

---
+++ AdminAPI/WebAPI/Controllers/Accountontroller.cs
@@ -0,0 +1,21 @@
+﻿using WebAPI.Models.DataManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Models.DataController
+{
+    [ApiController, Route("api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly AccountManager _repo;
+        public AccountController(AccountManager repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}")]
+        public Account GetAccount(int id)
+        {
+            return _repo.Get(id);
+        }
+    }
+}
---
+++ AdminAPI/WebAPI/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAPI.Models.DataManager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Models.DataController
+{
+    [ApiController, Route("api/[controller]")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly TransactionManager _repo;
+        public TransactionController(TransactionManager repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet("{id}"), HttpGet("{id}/{start}"), HttpGet("{id}/{start}/{end}")]
+        public IEnumerable<Transaction> GetTransactions(int id, DateTime? start = null, DateTime? end = null)
+        {
+            return _repo.GetTransactions(id, start, end);
+        }
+    }
+}
